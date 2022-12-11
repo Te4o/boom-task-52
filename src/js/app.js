7efd8c5b974d039e679da0e1067a051b90487b21
Stoyan Dimitrov <stoyan.jordanov94@gmail.com>
2022-12-11 09:08:11 +0200

    task-52: adding elements dynamically

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,24 @@
 import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
-  // This block will be executed once the page is loaded and ready
-
   const button = document.querySelector(".button");
-  button.addEventListener("click", () => {
-    alert("💣");
+  button.addEventListener("click", (event) => {
+    event.preventDefault();
   });
+});
+
+let addArticle = () => {
+  const articles = document.createElement("article");
+  articles.classList.add("message");
+  articles.innerHTML = "sample text";
+  document.body.appendChild(articles);
+};
+
+document.body.addEventListener("click", (event) => {
+  let count = 5;
+  while (count != 0) {
+    addArticle();
+    count--;
+  }
+  event.preventDefault();
 });
